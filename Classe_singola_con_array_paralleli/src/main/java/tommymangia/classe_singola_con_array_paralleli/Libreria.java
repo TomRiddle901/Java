d0fc4d40b2257e7ecf7af61d4b91918e2d6f12ee
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-11 14:59:28 +0100

    started project, added constructors

---
+++ Classe_singola_con_array_paralleli/src/main/java/tommymangia/classe_singola_con_array_paralleli/Libreria.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tommymangia.classe_singola_con_array_paralleli;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Libreria {
+    // Metodi
+    String nome;
+    String indirizzo;
+    String numeroTelefono;
+    String email;
+    String[] isbn;
+    String[] titolo;
+    String[] autori;
+    int[] copieDisponibili;
+    
+    // Costruttori
+    public Libreria(){
+        this.nome = "Libreria Default";
+        this.indirizzo = "Via Classe Java 20/J";
+        this.numeroTelefono = "1234567890";
+        this.email = "libreriadefault@librerie.com";
+    }
+    
+    public Libreria(String nome, String indirizzo, String numeroTelefono, String email){
+        this.nome=nome;
+        this.indirizzo=indirizzo;
+        this.numeroTelefono=numeroTelefono;
+        this.email=email;
+        isbn = new String[100];
+        titolo = new String[100];
+        autori = new String[100];
+        copieDisponibili = new int[100];
+    }
+    
+    public Libreria(Libreria library){
+        this.nome=library.nome;
+        this.indirizzo=library.indirizzo;
+        this.numeroTelefono=library.numeroTelefono;
+        this.email=library.email;
+        this.isbn=library.isbn;
+        this.titolo=library.titolo;
+        this.autori=library.autori;
+        this.copieDisponibili=library.copieDisponibili;
+    }
+}
