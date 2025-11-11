def254d9aee0645a1900dcf2fd813d1a3c8544d6
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-11 15:28:58 +0100

    renamed main class

--- Classe_singola_con_array_paralleli/src/main/java/tommymangia/classe_singola_con_array_paralleli/MainProgram.java
+++ Classe_singola_con_array_paralleli/src/main/java/tommymangia/classe_singola_con_array_paralleli/MainProgram.java
@@ -8,7 +8,7 @@ package tommymangia.classe_singola_con_array_paralleli;
  *
  * @author tommaso
  */
-public class Main {
+public class MainProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
