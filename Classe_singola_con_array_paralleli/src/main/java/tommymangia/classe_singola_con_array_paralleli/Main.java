2220b3a92fe2b81b1ad7b07f972d41a6175697e9
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-11 14:38:23 +0100

    new project

---
+++ Classe_singola_con_array_paralleli/src/main/java/tommymangia/classe_singola_con_array_paralleli/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tommymangia.classe_singola_con_array_paralleli;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
