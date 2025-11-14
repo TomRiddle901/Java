e1f58d38daa4a3e80bef68a33360e5d04ebeb753
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-14 11:07:15 +0100

    Added Methods

--- Esercizio_Squadra_Pallavolo/src/main/java/tommymangia/esercizio_squadra_pallavolo/Pallavolo.java
+++ Esercizio_Squadra_Pallavolo/src/main/java/tommymangia/esercizio_squadra_pallavolo/Pallavolo.java
@@ -14,6 +14,8 @@ public class Pallavolo {
     private String partitaIva;
     private int[] punteggi;
     private int partiteGiocate;
+    private int partiteVinte;
+    private int partitePerse;
     
     // Costruttori
     public Pallavolo(){
@@ -25,8 +27,12 @@ public class Pallavolo {
     public Pallavolo(String nome, String iva){
         this.nomeSquadra = nome;
         this.partitaIva = iva;
-        punteggi = new int[20];
+        punteggi = new int[22];
         this.partiteGiocate = 0;
+        
+        for (int i = 0; i < 22; i++){
+            punteggi[i] = -1;
+        }
     }
     
     public Pallavolo(Pallavolo p){
@@ -34,5 +40,46 @@ public class Pallavolo {
         this.partitaIva = p.partitaIva;
         this.punteggi = p.punteggi.clone();
         this.partiteGiocate = p.partiteGiocate;
+    }
+    
+    // Metodi
+  
+    // Getter
+    public String getNomeSquadra() {
+        return nomeSquadra;
+    }
+
+    public String getPartitaIva() {
+        return partitaIva;
+    }
+   
+    public int getPunteggioTotale(){
+        return 0;
+    }
+    
+    public int getPartitePerse(){
+        return 0;
+    }
+    
+    public int getPartiteGiocate(){
+        return partiteGiocate;
+    }
+    
+    // Setter
+
+    public void setNomeSquadra(String nomeSquadra) {
+        this.nomeSquadra = nomeSquadra;
+    }
+
+    public void setPartitaIva(String partitaIva) {
+        this.partitaIva = partitaIva;
+    }
+    
+    public void inserisciRisultato(int setVinti, int setPersi){
+        
+    }
+    
+    public String toString(){
+        return "";
     }
 }
