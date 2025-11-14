24d9198a6bae2629db08fbb75bc7e0b4bde4cd39
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-14 15:17:40 +0100

    written the MainProgram class

--- Esercizio_Squadra_Pallavolo/src/main/java/tommymangia/esercizio_squadra_pallavolo/MainProgram.java
+++ Esercizio_Squadra_Pallavolo/src/main/java/tommymangia/esercizio_squadra_pallavolo/MainProgram.java
@@ -11,6 +11,24 @@ package tommymangia.esercizio_squadra_pallavolo;
 public class MainProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Pallavolo squadra = new Pallavolo("Squadra1", "IVB5493SAD");
+        
+        System.out.println(squadra.getNomeSquadra());
+        System.out.println(squadra.getPartitaIva());
+        System.out.println(squadra.getPartiteGiocate());
+        System.out.println(squadra.getPartitePerse());
+        System.out.println(squadra.getPunteggioTotale());
+        System.out.println(squadra.toString());
+        
+        squadra.inserisciRisultato(3, 6);
+        System.out.println(squadra.toString());
+        
+        squadra.inserisciRisultato(5, 5);
+        squadra.inserisciRisultato(11, 0);
+        squadra.inserisciRisultato(5, 0);
+        squadra.inserisciRisultato(1, 3);
+        squadra.inserisciRisultato(5, 10);
+        
+        System.out.println(squadra.toString());
     }
 }
