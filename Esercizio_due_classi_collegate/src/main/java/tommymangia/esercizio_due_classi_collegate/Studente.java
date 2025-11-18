7f6da299a2b3a86f8b53683ca0349f12b2711cf1
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-18 18:47:05 +0100

    Added and implemented new methods

--- Esercizio_due_classi_collegate/src/main/java/tommymangia/esercizio_due_classi_collegate/Studente.java
+++ Esercizio_due_classi_collegate/src/main/java/tommymangia/esercizio_due_classi_collegate/Studente.java
@@ -93,4 +93,70 @@ public class Studente {
     public void setSesso(char sesso) {
         this.sesso = sesso;
     }
+    
+    // Inserimento voto
+    public boolean inserisciValutazione(Valutazione v){
+        int c = 0;
+        
+        while (c < 100 && this.valutazioni[c] != null){
+            c++;
+        }
+        
+        if (c != 100){
+            valutazioni[c] = v;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // Calcolo media generale
+    public double mediaGenerale(){
+        int c = 0;
+        double sVoti = 0;
+        
+        while (c < 100 && this.valutazioni[c] != null){
+            sVoti += this.valutazioni[c].getPunteggio();
+            c++;
+        }
+        
+        if (sVoti != 0){
+            return sVoti / c;
+        } else {
+            return 0;
+        }
+    }
+    
+    // Media per materia
+    public String mediaMateria(String cercaMateria){
+        int nVotiS = 0, nVotiO = 0, nVotiP = 0, c = 0;
+        double sVotiS = 0, sVotiO = 0, sVotiP = 0;
+        
+        while (c < 100 && this.valutazioni[c] != null){
+            if (cercaMateria.equals(valutazioni[c].getMateria())){
+                switch(valutazioni[c].getTipo()){
+                    case 'P':
+                        nVotiP++;
+                        sVotiP += valutazioni[c].getPunteggio();
+                    case 'O':
+                        nVotiO++;
+                        sVotiO += valutazioni[c].getPunteggio();
+                    case 'S':
+                        nVotiS++;
+                        sVotiS += valutazioni[c].getPunteggio();
+                }
+            }
+            c++;
+        }
+        
+        if (sVotiO != 0 && sVotiP != 0 && sVotiS != 0){
+            return "Media per la materia: " + cercaMateria
+                    + "\nAlunno: " + getNome() + " " + getCognome()
+                    + "\nMedia voti Orali: " + sVotiO / nVotiO
+                    + "\nMedia voti Pratici: " + sVotiP / nVotiP
+                    + "\nMedia voti Scritti: " + sVotiS / sVotiS;
+        } else {
+            return "Nessun voto inserito!";
+        }
+    }
 }
