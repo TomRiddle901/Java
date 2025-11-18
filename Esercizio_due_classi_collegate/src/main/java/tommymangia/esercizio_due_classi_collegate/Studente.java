89991f0061f13b8346487fd232e16f2e17bae07b
tommaso <tommaso.mangialardo@hotmail.com>
2025-11-18 17:38:45 +0100

    Added Fields and Constructors

--- Esercizio_due_classi_collegate/src/main/java/tommymangia/esercizio_due_classi_collegate/Studente.java
+++ Esercizio_due_classi_collegate/src/main/java/tommymangia/esercizio_due_classi_collegate/Studente.java
@@ -5,11 +5,47 @@
 package tommymangia.esercizio_due_classi_collegate;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 /**
  *
  * @author tommaso
  */
 public class Studente {
+    // Attributi
+    int matricola;
+    String nome;
+    String cognome;
+    LocalDate dataNascita;
+    char sesso;
+    static double costoOffertaFormativa;
+    Valutazione[] valutazioni;
     
+    // Costruttori
+    public Studente(){
+        matricola = 0;
+        nome = "NomeDefault";
+        cognome = "CognomeDefault";
+        sesso = 'F';
+        dataNascita = LocalDate.of(2025, 11, 18);
+        valutazioni = new Valutazione[100];
+    }
+    
+    public Studente(int matricola, String nome, String cognome, LocalDate dataNascita, char sesso){
+        this.matricola = matricola;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.sesso = sesso;
+        this.valutazioni = new Valutazione[100];
+    }
+    
+    public Studente(Studente s){
+        this.matricola = s.matricola;
+        this.nome = s.nome;
+        this.cognome = s.cognome;
+        this.dataNascita = s.dataNascita;
+        this.sesso = s.sesso;
+        this.valutazioni = s.valutazioni.clone();
+    }
 }
