958997a33413af8297caa30b9043f4cb9de392c2
TommyMangia <tommaso.mangialardo@hotmail.com>
2025-11-20 16:33:08 +0100

    Added fields and constructors

--- Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/Punto.java
+++ Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/Punto.java
@@ -9,5 +9,23 @@ package tommymangia.triangolo_con_classe_punto;
  * @author tommaso
  */
 public class Punto {
+    // Abbributi
+    private double x;
+    private double y;
+    
+    // Costruttori
+    public Punto(){
+        
+    }
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Punto(Punto p){
+        this.x = p.x;
+        this.y = p.y;
+    }
     
 }
--- Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/Triangolo.java
+++ Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/Triangolo.java
@@ -9,5 +9,26 @@ package tommymangia.triangolo_con_classe_punto;
  * @author tommaso
  */
 public class Triangolo {
+    // Attributi
+    private Punto a;
+    private Punto b;
+    private Punto c;
     
+    // Costruttori
+
+    public Triangolo(){
+        
+    }
+
+    public Triangolo(Punto a, Punto b, Punto c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    
+    public Triangolo(Triangolo t){
+        this.a = t.a;
+        this.b = t.b;
+        this.c = t.c;
+    }
 }
