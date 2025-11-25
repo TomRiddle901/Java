4bddcee61f64e7c166e138e96d182391836d2052
TommyMangia <tommaso.mangialardo@hotmail.com>
2025-11-25 14:46:31 +0100

    Try... catch funcions test

--- Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/MainProgram.java
+++ Triangolo_con_classe_punto/src/main/java/tommymangia/triangolo_con_classe_punto/MainProgram.java
@@ -4,6 +4,8 @@
 
 package tommymangia.triangolo_con_classe_punto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tommaso
@@ -11,6 +13,28 @@ package tommymangia.triangolo_con_classe_punto;
 public class MainProgram {
 
     public static void main(String[] args) {
+        int scelta;
         
+        scelta = visualizzaMenu();
+        System.out.println("Hai scelto " + scelta);
+    }
+    
+    public static int visualizzaMenu(){
+        int scelta;
+        Scanner in = new Scanner(System.in);
+        System.out.println("1) Crea Triagolo");
+        System.out.println("2) Visualizza Area");
+        System.out.println("3) Visualizza Perimetro");
+        System.out.println("4) Trasla");
+        System.out.println("5) Informazioni");
+        System.out.println("0) Uscita");
+        System.out.print("Scelta: ");
+        try{
+            scelta = Integer.parseInt(in.nextLine());
+        } catch(NumberFormatException e){
+            System.out.println("Opzione non consentita!");
+            return -1;
+        }
+        return scelta;
     }
 }
