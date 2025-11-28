ac82d737bfc026e3b737f9c5f5187f8f3f9074c1
TommyMangia <tommaso.mangialardo@hotmail.com>
2025-11-28 10:51:28 +0100

    Menu and First method

--- Poligono_con_classe_Punto/src/main/java/tommymangia/poligono_con_classe_punto/MainProgram.java
+++ Poligono_con_classe_Punto/src/main/java/tommymangia/poligono_con_classe_punto/MainProgram.java
@@ -4,6 +4,8 @@
 
 package tommymangia.poligono_con_classe_punto;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tommaso
@@ -11,6 +13,82 @@ package tommymangia.poligono_con_classe_punto;
 public class MainProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int scelta;
+        
+        do{
+            scelta = menu();
+            
+            switch(scelta){
+                case 1:
+                    creaPoligono();
+                    break;
+                case 2:
+                    System.out.println("Funzione non implementata!");
+                case 3:
+                    System.out.println("Funzione non implementata");
+                case 0:
+                    System.out.println("Uscita dal programma in corso...");
+                default:
+                    System.out.println("Scelta non valida!");
+            }
+        } while(scelta != 0);
+    }
+    
+    public static int menu(){
+        int scelta;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Poligono!");
+        System.out.println("1) Crea Poligono");
+        System.out.println("2) Perimetro del Poligono");
+        System.out.println("3) Area del Poligono");
+        System.out.println("0) Esci");
+        System.out.print("Scelta: ");
+        
+        try{
+            scelta = Integer.parseInt(in.nextLine());
+        } catch (NumberFormatException ex) {
+            System.err.println("Errore: " + ex.getMessage());
+        }
+        
+        return scelta;
+    }
+    
+    public static void creaPoligono(){
+        Scanner in = new Scanner(System.in);
+        String lato;
+        int latoI;
+        double x, y;
+        
+        try{
+            System.out.print("Inserisci il numero dei lati: ");
+            lato = in.nextLine();
+            latoI = Integer.parseInt(lato);
+            
+            if (latoI < 3){
+                throw new IllegalArgumentException("Un poligono deve avere 3 o più lati!");
+            }
+            
+            Punto[] aP = new Punto[latoI];
+            
+            for (int i = 0; i < latoI; i++){
+                System.out.print("Inserisci la coordinata X del " + (i + 1) + " punto: ");
+                lato = in.nextLine();
+                x = Double.parseDouble(lato);
+                
+                System.out.print("Inserisci la coordinata Y del " + (i + 1) + " punto: ");
+                lato = in.nextLine();
+                y = Double.parseDouble(lato);
+                
+                aP[i] = new Punto(x, y);
+            }
+            
+            Poligono pol = new Poligono(latoI, aP);
+        } catch (NumberFormatException ex) {
+            System.err.println("Errore: Devi inserire un numero valido!");
+            System.err.println("Più info: " + ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.err.println("Errore: " + ex.getMessage());
+        }
     }
 }
