f7c118b8571ae0e11dd319bf29988cf2bf86877d
Tommaso <tommaso.mangialardo@hotmail.com>
2025-12-14 10:55:36 +0100

    Array di punti non sullo stesso asse

--- Poligono_con_classe_Punto/src/main/java/tommymangia/poligono_con_classe_punto/Poligono.java
+++ Poligono_con_classe_Punto/src/main/java/tommymangia/poligono_con_classe_punto/Poligono.java
@@ -13,14 +13,20 @@ public class Poligono {
     private int numeroPunti;
     private Punto[] punti;
     
-    // Costruttori
-    public Poligono(){
-        
-    }
-    
+    // Costruttori    
     public Poligono(int numeroPunti, Punto[] punti){
+        
+        if (puntiCoallineati(punti, numeroPunti)){
+            throw new IllegalArgumentException(
+                "Errore: tre o più punti sono allineati"
+            );
+        }
         this.numeroPunti = numeroPunti;
         this.punti = new Punto[numeroPunti];
+        
+        for (int i = 0; i < numeroPunti; i++){
+            this.punti[i] = punti[i];
+        }
     }
     
     // Metodi
@@ -42,8 +48,25 @@ public class Poligono {
         }
         
         // Ultimo lato che chiude il poligono
-        perimetro = punti[numeroPunti - 1].distanzaPunti(punti[0]);
+        perimetro += punti[numeroPunti - 1].distanzaPunti(punti[0]);
         
         return perimetro;
+    }
+    
+    private boolean puntiCoallineati(Punto[] p, int n){
+        for (int i = 0; i < n; i++){;
+            for (int j = i + 1; j < n; j++){
+                for (int k = j + 1; k < n; k++){
+                    double det = (p[j].getX() - p[i].getX()) * (p[k].getY() - p[i].getY()) - 
+                                 (p[j].getY() - p[i].getY()) * (p[k].getX() - p[i].getX());
+                    
+                    if (det == 0){
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        return false;
     }
 }
