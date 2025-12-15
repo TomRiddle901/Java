9be33efeb4bfee2b54bc6f67c9770aade38c0c28
Tommaso <tommaso.mangialardo@hotmail.com>
2025-12-15 17:09:16 +0100

    New Project

---
+++ Applicazione_Biblioteca/src/main/java/com/tommymangia/applicazione_biblioteca/MainProgram.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tommymangia.applicazione_biblioteca;
+
+/**
+ *
+ * @author tommaso
+ */
+public class MainProgram {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
