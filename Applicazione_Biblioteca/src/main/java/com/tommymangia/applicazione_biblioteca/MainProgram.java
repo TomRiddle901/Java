4ac2a7151d3a548cadd66b6d535d859c8c09a03b
Tommaso <tommaso.mangialardo@hotmail.com>
2025-12-17 18:52:49 +0100

    Solved a bug

--- Applicazione_Biblioteca/src/main/java/com/tommymangia/applicazione_biblioteca/MainProgram.java
+++ Applicazione_Biblioteca/src/main/java/com/tommymangia/applicazione_biblioteca/MainProgram.java
@@ -16,9 +16,11 @@ public class MainProgram {
     private static Biblioteca b = new Biblioteca("Gianni", "Via Indirizzo 15/B", "www.giannilibrary.com", "giaani@library.com", "+39 1234567890");
 
     public static void main(String[] args) {
-        int scelta = menu();
+        int scelta;
         
         do{
+            scelta = menu();
+            
             switch (scelta) {
                 case 1:
                     b.addLibro();
