5916b7ccf67d9848d59aefb6d908e21866215676
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-10 18:38:57 +0100

    Fields and Constructors

---
+++ Esercizio_recupero_2/src/main/java/com/tommymangia/esercizio_recupero_2/Chiamate.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tommymangia.esercizio_recupero_2;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Chiamate {
+    // Attributi
+    private String numeroTelefono;
+    private LocalTime durata;
+    
+    // Construttori
+    public Chiamate(String numeroChiamato, LocalTime durataChiamata){
+        this.numeroTelefono = numeroChiamato;
+        this.durata = durataChiamata;
+    }
+}
