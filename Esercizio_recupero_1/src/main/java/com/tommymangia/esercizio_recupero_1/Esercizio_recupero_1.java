8f2ca9b8dfd99b56647883c847783989ba35eb0e
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-10 10:50:02 +0100

    New Project

---
+++ Esercizio_recupero_1/src/main/java/com/tommymangia/esercizio_recupero_1/Esercizio_recupero_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tommymangia.esercizio_recupero_1;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Esercizio_recupero_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Esercizio_recupero_1/src/main/java/com/tommymangia/esercizio_recupero_1/Sim.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tommymangia.esercizio_recupero_1;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Sim {
+    
+}
