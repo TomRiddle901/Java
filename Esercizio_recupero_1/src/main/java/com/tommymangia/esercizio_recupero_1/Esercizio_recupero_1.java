e9c138a36785a906f3171d57cc0db6b68379070a
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-10 11:07:46 +0100

    Menù

--- Esercizio_recupero_1/src/main/java/com/tommymangia/esercizio_recupero_1/Esercizio_recupero_1.java
+++ Esercizio_recupero_1/src/main/java/com/tommymangia/esercizio_recupero_1/Esercizio_recupero_1.java
@@ -4,13 +4,32 @@
 
 package com.tommymangia.esercizio_recupero_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tommaso
  */
 public class Esercizio_recupero_1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Sim sim;
+    
+    public static void main(String[] args){
+        
+    }
+    
+    private int menu(){
+        Scanner in = new Scanner(System.in);
+        int scelta;
+        
+        System.out.println("+------Menù------+");
+        System.out.println("1) Aggiungi SIM");
+        System.out.println("2) Aggiungi chiamata");
+        System.out.println("3) Visualizza dettagli SIM");
+        System.out.println("0) Esci");
+        System.out.print("Scelta: ");
+        scelta = in.nextInt();
+        
+        return scelta;
     }
 }
