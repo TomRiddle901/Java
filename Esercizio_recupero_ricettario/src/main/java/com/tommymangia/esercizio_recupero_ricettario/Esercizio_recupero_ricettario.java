481510e7b38deb5eb9d039b3bd81cbbd13ee4309
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-19 16:10:39 +0100

    Menu

--- Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Esercizio_recupero_ricettario.java
+++ Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Esercizio_recupero_ricettario.java
@@ -4,13 +4,34 @@
 
 package com.tommymangia.esercizio_recupero_ricettario;
 
+import java.util.Scanner;
+import java.time.LocalTime;
+
 /**
  *
  * @author tommaso
  */
 public class Esercizio_recupero_ricettario {
+    
+    private Ricetta ricette[];
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+    
+    private int menu(){
+        Scanner in = new Scanner(System.in);
+        int scelta;
+        
+        System.out.println("Menu");
+        System.out.println("1) Inserisci Ricetta");
+        System.out.println("2) Elenco allergeni di una ricetta");
+        System.out.println("3) Aggiungi immagine alla ricetta");
+        System.out.println("4) Crea pagina HTML");
+        System.out.println("0) Esci");
+        System.out.print("Scelta: ");
+        scelta = in.nextInt();
+        
+        return scelta;
     }
 }
