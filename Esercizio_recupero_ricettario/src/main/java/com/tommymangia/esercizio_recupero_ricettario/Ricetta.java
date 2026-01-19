e3802d703709478feb47f6c0543ceb0d3a6262db
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-19 15:47:47 +0100

    Fields and constructors

--- Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ricetta.java
+++ Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ricetta.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.tommymangia.esercizio_recupero_ricettario;
+import java.time.LocalTime;
 
 /**
  *
  * @author tommaso
  */
 public class Ricetta {
+    // Attributi
+    private String nomeRicetta;
+    private LocalTime tempoPreparazione;
+    private String descrizionePreparazione;
+    private Ingrediente[] ingredienti;
+    private String pathImmagine;
     
+    // Costruttore
+    public Ricetta(String nome, LocalTime tempoPreparazione){
+        this.nomeRicetta = nome;
+        this.tempoPreparazione = tempoPreparazione;
+        ingredienti = new Ingrediente[64];
+        pathImmagine = null;
+        descrizionePreparazione = null;
+    }
 }
