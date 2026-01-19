63fde2869199ea6bef1ff8c989f3fae2dfd947a3
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-19 15:32:50 +0100

    New Project

---
+++ Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Esercizio_recupero_ricettario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tommymangia.esercizio_recupero_ricettario;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Esercizio_recupero_ricettario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
