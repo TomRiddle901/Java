dbd0a3508b96edf7bf7b561fd5ba8fcc84a938ce
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-19 15:47:56 +0100

    Fields and constructors

--- Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ingrediente.java
+++ Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ingrediente.java
@@ -9,5 +9,17 @@ package com.tommymangia.esercizio_recupero_ricettario;
  * @author tommaso
  */
 public class Ingrediente {
+    // Attributi
+    private String nome;
+    private float quantita;
+    private char unitaMisura;
+    private String allergene;
     
+    // Costruttore
+    public Ingrediente(String nome, float quantita, char unitaMisura, String allergene){
+        this.nome = nome;
+        this.quantita = quantita;
+        this.unitaMisura = unitaMisura;
+        this.allergene = allergene;
+    }
 }
