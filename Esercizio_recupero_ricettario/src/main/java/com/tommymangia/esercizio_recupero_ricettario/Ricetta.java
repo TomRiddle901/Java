b2cdd6de5412ba6059a448ab100464efa735c7ce
Tommaso <tommaso.mangialardo@hotmail.com>
2026-01-25 12:00:38 +0100

    Formatting html...

--- Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ricetta.java
+++ Esercizio_recupero_ricettario/src/main/java/com/tommymangia/esercizio_recupero_ricettario/Ricetta.java
@@ -85,9 +85,9 @@ public class Ricetta {
         
         output.append("         <p>Nome ricetta: ")
               .append(nomeRicetta)
-              .append("         \n<br>Tempo di preparazione: ")
+              .append("\n         <br>Tempo di preparazione: ")
               .append(tempoPreparazione)
-              .append("         \n<br>Descrizione preparazione:<br>")
+              .append("\n         <br>Descrizione preparazione:<br>")
               .append(descrizionePreparazione)
               .append("</p>\n")
               .append("         <h2>Ingredienti</h2>\n")
