072ad3a29be546fd25795ea926a8aeb6ebc8c27a
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-02 18:44:13 +0100

    Refactor Manager and Programmatore classes to use ArrayList for sviluppatore and linguaggi attributes

--- Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/Manager.java
+++ Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/Manager.java
@@ -5,13 +5,14 @@
 package tommymangia.esercizio_diependenti;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  *
  * @author Tommaso
  */
 public class Manager extends Dipendente{
-    private Programmatore[] sviluppatore;
+    private ArrayList<Programmatore> sviluppatore;
     
     public Manager(String codice, String nome, String cognome, LocalDate dataAssunzione, double stipendioBase){
         super(codice, nome, cognome, dataAssunzione, stipendioBase);
--- Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/Programmatore.java
+++ Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/Programmatore.java
@@ -5,13 +5,14 @@
 package tommymangia.esercizio_diependenti;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 /**
  *
  * @author Tommaso
  */
 public class Programmatore extends Dipendente{
-    private String[] linguaggi;
+    private ArrayList<String> linguaggi;
     
     public Programmatore(String codice, String nome, String cognome, LocalDate dataAssunzione, double stipendioBase){
         super(codice, nome, cognome, dataAssunzione, stipendioBase);
