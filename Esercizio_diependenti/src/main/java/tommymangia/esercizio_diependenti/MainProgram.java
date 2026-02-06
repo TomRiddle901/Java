0e2f6d61183e083fe929d67ca51d579b1b5a3754
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-06 10:58:22 +0100

    Created the principal menu

--- Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/MainProgram.java
+++ Esercizio_diependenti/src/main/java/tommymangia/esercizio_diependenti/MainProgram.java
@@ -4,6 +4,9 @@
 
 package tommymangia.esercizio_diependenti;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.time.LocalDate;
 /**
  *
  * @author Tommaso
@@ -12,5 +15,22 @@ public class MainProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+    
+    public int menu(){
+        int scelta;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Menù");
+        System.out.println("1) Aggiungi dipendente");
+        System.out.println("2) Assegna un manager ad un programmatore");
+        System.out.println("3) Modifica percentuali ed il contributo");
+        System.out.println("4) Calcola stipendio di un dipendente");
+        System.out.println("0) Esci");
+        System.out.print("Scelta: ");
+        
+        scelta = in.nextInt();
+        
+        return scelta;
     }
 }
