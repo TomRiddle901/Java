e5a90a91e2d467524c57c123ef179061fedad6b8
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-17 18:07:17 +0100

    Implemented method for calculate a salary (not monthly for now) and a method for update the extra hours work salary

--- Esercizio_dipendenti2/src/main/java/org/tommymangia/Tecnici.java
+++ Esercizio_dipendenti2/src/main/java/org/tommymangia/Tecnici.java
@@ -9,4 +9,12 @@ public class Tecnici extends Dipendente{
     public Tecnici(String matricola, String nome, String cognome, double stipendioBase){
         super(matricola, nome, cognome, stipendioBase);
     }
+
+    public double calcolaStipendio(){
+        return stipendioBase + importoStraordinari * oreStaordinari.getHour();
+    }
+
+    public static void aggiornaStraordinari(double nuovoImporto) {
+        importoStraordinari = nuovoImporto;
+    }
 }
