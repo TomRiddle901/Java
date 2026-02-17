c7de326baa02934dc51992c8cc0fe69415277272
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-17 18:18:29 +0100

    Created ArrayList of dipendenti, and printed the result of calcolaStipendio()

--- Esercizio_dipendenti2/src/main/java/org/tommymangia/Main.java
+++ Esercizio_dipendenti2/src/main/java/org/tommymangia/Main.java
@@ -1,7 +1,26 @@
 package org.tommymangia;
 
+import java.lang.foreign.SymbolLookup;
+import java.util.ArrayList;
+
 public class Main {
+
+    private static ArrayList<Dipendente> listaDipendenti;
+
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        Amministrativi amm = new Amministrativi("amm1", "Ammi", "Strativi", 2000, 10);
+        Amministrativi ammDue = new Amministrativi("amm2", "Strativi", "Ammi", 1000, 10);
+        Tecnici tec = new Tecnici("tec1", "Tec", "Nici", 1500.10);
+        Tecnici tecDue = new Tecnici("tec2", "Nici", "Tec", 1000.15);
+
+        listaDipendenti.add(amm);
+        listaDipendenti.add(ammDue);
+        listaDipendenti.add(tec);
+        listaDipendenti.add(tecDue);
+
+        System.out.println(amm.calcolaStipendio());
+        System.out.println(ammDue.calcolaStipendio());
+        System.out.println(tec.calcolaStipendio());
+        System.out.println(tecDue.calcolaStipendio());
     }
 }
