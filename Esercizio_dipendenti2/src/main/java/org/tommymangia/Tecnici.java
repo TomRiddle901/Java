a59d3f6321cb597dfb298d361add3c8b3676ffdd
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-17 18:10:43 +0100

    changed oreStraordinari from LocalTime to int

--- Esercizio_dipendenti2/src/main/java/org/tommymangia/Tecnici.java
+++ Esercizio_dipendenti2/src/main/java/org/tommymangia/Tecnici.java
@@ -4,17 +4,21 @@ import java.time.LocalTime;
 
 public class Tecnici extends Dipendente{
     private static double importoStraordinari = 10;
-    private LocalTime oreStaordinari;
+    private int oreStaordinari;
 
     public Tecnici(String matricola, String nome, String cognome, double stipendioBase){
         super(matricola, nome, cognome, stipendioBase);
     }
 
     public double calcolaStipendio(){
-        return stipendioBase + importoStraordinari * oreStaordinari.getHour();
+        return stipendioBase + importoStraordinari * oreStaordinari;
     }
 
     public static void aggiornaStraordinari(double nuovoImporto) {
         importoStraordinari = nuovoImporto;
+    }
+
+    public void aggiornaOreStraordinari(int nuoveOre){
+        oreStaordinari += nuoveOre;
     }
 }
