827680f5972ee671a9ffbf0746669821c820aabc
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-20 11:20:28 +0100

    Implemented menu function

--- Esercizio_classe_veicoli/src/main/java/MainProgram.java
+++ Esercizio_classe_veicoli/src/main/java/MainProgram.java
@@ -3,7 +3,19 @@ import java.util.Scanner;
 
 public class MainProgram {
 
+    private static ArrayList<Veicolo> listaVeicoli = new ArrayList<>();
+
     public static void main(String[] args){
 
+    }
+
+    private static int menu(){
+        int scelta;
+
+        System.out.println("Menù");
+        System.out.println("1) Aggiungi un veicolo");
+        System.out.println("2) Calcola consumo per 100Km");
+        System.out.println("3) Stampa informazioni veicolo");
+        System.out.println("0) Esci");
     }
 }
