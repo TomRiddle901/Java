eb58de5c4cfe1fdcf3583d454d611f7ea20ec146
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-20 11:27:19 +0100

    Added switch for selecting a function using the menu

--- Esercizio_classe_veicoli/src/main/java/MainProgram.java
+++ Esercizio_classe_veicoli/src/main/java/MainProgram.java
@@ -6,7 +6,27 @@ public class MainProgram {
     private static ArrayList<Veicolo> listaVeicoli = new ArrayList<>();
 
     public static void main(String[] args){
+        int scelta;
 
+        do{
+            scelta = menu();
+            switch (scelta){
+                case 1:
+                    aggiungiVeicolo();
+                    break;
+                case 2:
+                    calcolaConsumoCentoChilometri();
+                    break;
+                case 3:
+                    infoVeicolo();
+                    break;
+                case 0:
+                    System.out.println("UScita dal programma in corso...");
+                    break;
+                default:
+                    System.out.println("Scelta non valida!");
+            }
+        } while(scelta != 0);
     }
 
     private static int menu(){
