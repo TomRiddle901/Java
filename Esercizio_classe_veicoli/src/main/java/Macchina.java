5cf7eee6b7177c1b3ba4955cde4e127c5a9fd8e5
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-20 11:01:03 +0100

    Implemented Macchina class with fields and Constructor

--- Esercizio_classe_veicoli/src/main/java/Macchina.java
+++ Esercizio_classe_veicoli/src/main/java/Macchina.java
@@ -1,2 +1,12 @@
-public class Macchina {
+public class Macchina extends Veicolo{
+    private int numeroPosti;
+    private String tipoCarburante;
+    private double consumoCentoChilometri;
+
+    public Macchina(String marca, String modello, int numeroPosti, String tipoCarburante, double consumoCentoChilometri){
+        super(marca, modello);
+        this.numeroPosti = numeroPosti;
+        this.tipoCarburante = tipoCarburante;
+        this.consumoCentoChilometri = consumoCentoChilometri;
+    }
 }
