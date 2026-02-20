abc01714b94a046e0a086b776bab83222d5abb49
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-20 11:03:41 +0100

    Implemented Moto class with fields and Constructor

--- Esercizio_classe_veicoli/src/main/java/Moto.java
+++ Esercizio_classe_veicoli/src/main/java/Moto.java
@@ -1,2 +1,8 @@
-public class Moto {
+public class Moto extends Veicolo{
+    private double consumoCentoChilometri;
+
+    public Moto(String marca, String modello, double consumoCentoChilometri){
+        super(marca, modello);
+        this.consumoCentoChilometri = consumoCentoChilometri;
+    }
 }
