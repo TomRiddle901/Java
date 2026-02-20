e22ca4c3d171a59bfc8a1a78b9268731f54f0a34
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-20 10:58:27 +0100

    Implemented class Veicolo.java with constructor Veicolo, abstract function for calculate consumption and info function

--- Esercizio_classe_veicoli/src/main/java/Veicolo.java
+++ Esercizio_classe_veicoli/src/main/java/Veicolo.java
@@ -1,2 +1,15 @@
-public class Veicolo {
+public abstract class Veicolo {
+    protected String marca;
+    protected String modello;
+
+    protected Veicolo(String marca, String modello){
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    protected abstract double calcolaConsumo(double km);
+
+    public String info(){
+        return "";
+    }
 }
