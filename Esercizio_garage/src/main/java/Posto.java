5121d4df9e91a00e83c3cb04ca39cc2329b5e3c1
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 17:07:17 +0100

    Adedd function for calculate the hourly rate

--- Esercizio_garage/src/main/java/Posto.java
+++ Esercizio_garage/src/main/java/Posto.java
@@ -1,25 +1,37 @@
 import java.time.LocalTime;
+import java.time.Duration;
 
 public class Posto {
     private boolean riservato;
     private LocalTime inizioSosta;
     private Veicolo veicolo;
 
-    public Posto(boolean riservato, LocalTime inizioSosta){
+    public Posto(boolean riservato, LocalTime inizioSosta) {
         this.riservato = riservato;
         this.inizioSosta = inizioSosta;
         this.veicolo = null;
     }
 
-    public void inserisciVeicolo(String targa, String tipo){
-        if (tipo.equals("A") || tipo.equals("a")){
+    public void inserisciVeicolo(String targa, String tipo) {
+        if (tipo.equals("A") || tipo.equals("a")) {
             veicolo = new Macchina(targa);
-        } else if (tipo.equals("M") || tipo.equals("m")){
+        } else if (tipo.equals("M") || tipo.equals("m")) {
             veicolo = new Motocicletta(targa);
-        }else if (tipo.equals("F") || tipo.equals("f")){
+        } else if (tipo.equals("F") || tipo.equals("f")) {
             veicolo = new Furgone(targa);
-        }else {
+        } else {
             System.out.println("Veicolo inesistente!");
         }
+    }
+
+    public double calcolaTariffaOraria(LocalTime fineSosta) {
+        double tariffaTotale = 0;
+        Duration oreSosta = null;
+
+        oreSosta = Duration.between(inizioSosta, fineSosta);
+
+        tariffaTotale = veicolo.getTariffaOraria() * oreSosta.toHours();
+
+        return tariffaTotale;
     }
 }
