fe3c93920b59188907e82a85015eb4f83e8b428e
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 16:43:18 +0100

    Implemented basic function for add a Veicolo

--- Esercizio_garage/src/main/java/Posto.java
+++ Esercizio_garage/src/main/java/Posto.java
@@ -3,9 +3,23 @@ import java.time.LocalTime;
 public class Posto {
     private boolean riservato;
     private LocalTime inizioSosta;
+    private Veicolo veicolo;
 
     public Posto(boolean riservato, LocalTime inizioSosta){
         this.riservato = riservato;
         this.inizioSosta = inizioSosta;
+        this.veicolo = null;
+    }
+
+    public void inserisciVeicolo(String targa, String tipo){
+        if (tipo.equals("A") || tipo.equals("a")){
+            veicolo = new Macchina(targa);
+        } else if (tipo.equals("M") || tipo.equals("m")){
+            veicolo = new Motocicletta(targa);
+        }else if (tipo.equals("F") || tipo.equals("f")){
+            veicolo = new Furgone(targa);
+        }else {
+            System.out.println("Veicolo inesistente!");
+        }
     }
 }
