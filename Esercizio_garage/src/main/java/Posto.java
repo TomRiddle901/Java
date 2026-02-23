375dcf02acf7f0e35d9d9783ff2746995333f9ed
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 16:29:14 +0100

    Fields and constructor for Posto.java

--- Esercizio_garage/src/main/java/Posto.java
+++ Esercizio_garage/src/main/java/Posto.java
@@ -1,2 +1,11 @@
+import java.time.LocalTime;
+
 public class Posto {
+    private boolean riservato;
+    private LocalTime inizioSosta;
+
+    public Posto(boolean riservato, LocalTime inizioSosta){
+        this.riservato = riservato;
+        this.inizioSosta = inizioSosta;
+    }
 }
