4cc67e8af0b9c02b1e5922286af080d95d61d9d7
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 16:25:06 +0100

    Fields for Garage.java

--- Esercizio_garage/src/main/java/Garage.java
+++ Esercizio_garage/src/main/java/Garage.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Garage {
+    private Posto[] posti;
+    private ArrayList<Giornata> listaGiornate;
+
+    public Garage(){
+        this.posti = new Posto[100];
+        this.listaGiornate = new ArrayList<>();
+    }
 }
