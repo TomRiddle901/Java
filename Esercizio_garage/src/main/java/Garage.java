af0f2629f21f218025882f6a2fddbd1537507c61
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 16:51:06 +0100

    Implemented a basic function to assign a seat to a vehicle

--- Esercizio_garage/src/main/java/Garage.java
+++ Esercizio_garage/src/main/java/Garage.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.time.LocalTime;
+import java.time.LocalDate;
 
 public class Garage {
     private Posto[] posti;
@@ -7,5 +9,21 @@ public class Garage {
     public Garage(){
         this.posti = new Posto[100];
         this.listaGiornate = new ArrayList<>();
+    }
+
+    public void inserisciMacchina(String targa, String tipoDiVeicolo, int posto, boolean riservato, LocalTime ingresso){
+        if (posti[posto] != null){
+            posti[posto] = new Posto(riservato, ingresso);
+            posti[posto].inserisciVeicolo(targa, tipoDiVeicolo);
+        } else{
+            System.out.println("Il posto selezionato è già occupato.");
+            System.out.println("Verrai asseganto ad un altro posto automaticamente!");
+            for (int i = 0; i < 100; i++){
+                if (posti[i] != null){
+                    posti[i] = new Posto(riservato, ingresso);
+                    posti[i].inserisciVeicolo(targa, tipoDiVeicolo);
+                }
+            }
+        }
     }
 }
