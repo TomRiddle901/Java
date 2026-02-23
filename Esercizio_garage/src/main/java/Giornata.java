02e8fff4ae04ce51a66e01ea3915e517156e05ec
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-23 16:27:24 +0100

    Fields and constructor for Giornata.java

--- Esercizio_garage/src/main/java/Giornata.java
+++ Esercizio_garage/src/main/java/Giornata.java
@@ -1,2 +1,11 @@
+import java.time.LocalDate;
+
 public class Giornata {
+    private LocalDate dataGiornata;
+    private double importoTotale;
+
+    public Giornata(LocalDate dataGiornata, double importoTotale){
+        this.dataGiornata = dataGiornata;
+        this.importoTotale = importoTotale;
+    }
 }
