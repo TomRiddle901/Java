016a544036e823104120ce827a18eefd72e9a18e
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-24 17:21:08 +0100

    Implemented function to insert a vehicle into the garage

--- Esercizio_garage/src/main/java/MainProgram.java
+++ Esercizio_garage/src/main/java/MainProgram.java
@@ -46,7 +46,45 @@ public class MainProgram {
     }
 
     private static void ingresso(){
+        Scanner in = new Scanner(System.in);
+        int posto;
+        String targa, tipoDiVeicolo, tipoVeicoloRiservato, isRiservato, postoCheck;
+        boolean riservato = false;
 
+        System.out.print("Targa: ");
+        targa = in.nextLine();
+        System.out.print("Tipo di veicolo (A: Auto, M: Motocicletta, F: Furgone): ");
+        tipoDiVeicolo = in.nextLine();
+        System.out.print("Posto riservato? (Si/No)");
+        isRiservato = in.nextLine();
+
+        if (isRiservato.equals("Si") || isRiservato.equals("si")){
+            riservato = true;
+            System.out.print("Tipo veicolo riservato (Ambulanza, Polizia, ...): ");
+            tipoVeicoloRiservato = in.nextLine();
+        }else if (isRiservato.equals("No") || isRiservato.equals("no")){
+            riservato = false;
+            tipoVeicoloRiservato = null;
+        }else {
+            System.out.println("Non valido, il posto non sarà riservato");
+            riservato = false;
+            tipoVeicoloRiservato = null;
+        }
+
+        System.out.println(garage.getPostiLiberi());
+        System.out.print("Scegli un posto libero (vuoto per selezione automatica): ");
+        postoCheck = in.nextLine();
+
+        if (postoCheck.equals("")){
+            System.out.println("Assegnazione automatica selezionata!");
+            posto = -1;
+        }else{
+            posto = Integer.parseInt(postoCheck);
+        }
+
+        garage.inserisciMacchina(targa, tipoDiVeicolo, tipoVeicoloRiservato, posto, riservato, LocalDateTime.now());
+
+        System.out.println("Ingresso aggiunto correttamente!");
     }
 
     private static void uscita(){
