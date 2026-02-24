53c56c6ac07c1c99f4ba4a02e72f85c9ed1b58b3
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-02-24 14:40:16 +0100

    Added menu function

--- Esercizio_garage/src/main/java/MainProgram.java
+++ Esercizio_garage/src/main/java/MainProgram.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+
 public class MainProgram {
 
     private static Garage garage = new Garage();
 
     public static void main(String[] args){
 
+    }
+
+    private static int menu(){
+        int scelta = 0;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Menu");
+        System.out.println("1) Ingresso");
+        System.out.println("2) Uscita");
+        System.out.println("3) visualizza incassi di una giornata");
+        System.out.print("Scelta: ");
+        scelta = in.nextInt();
+
+        return scelta;
     }
 }
