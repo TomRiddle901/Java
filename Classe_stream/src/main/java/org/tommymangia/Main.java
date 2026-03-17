055d2c4e0d036e43e181de60bae5bb30cb5f9016
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-17 14:54:45 +0100

    Added try/catch for better control of errors

--- Classe_stream/src/main/java/org/tommymangia/Main.java
+++ Classe_stream/src/main/java/org/tommymangia/Main.java
@@ -7,11 +7,16 @@ import java.io.IOException;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main() throws IOException {
+    public static void main(){
         Punto p = new Punto(3.78, 8.67, "A");
-        FileWriter f = new FileWriter("out.txt");
-        BufferedWriter bf = new BufferedWriter(f);
-        bf.write(p.toString());
-        bf.close();
+        try{
+            FileWriter f = new FileWriter("out.txt");
+            BufferedWriter bf = new BufferedWriter(f);
+            bf.write(p.toString());
+            bf.close();
+        }catch (IOException ex){
+            System.err.println("Errore input/output: " + ex.getMessage());
+        }
+
     }
 }
