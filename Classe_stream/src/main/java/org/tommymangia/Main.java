1967b34c66ae11b3b41ce7fa45637b3a6f045d8b
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-17 14:37:16 +0100

    New project

---
+++ Classe_stream/src/main/java/org/tommymangia/Main.java
@@ -0,0 +1,10 @@
+package org.tommymangia;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main() {
+        IO.println(String.format("Hello and welcome!"));
+
+    }
+}
