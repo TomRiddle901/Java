b985f7e9f28a2ba62b4ce798ef9caa06295e6555
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-17 18:12:02 +0100

    Removed useless comment

--- Classe_stream/src/main/java/org/tommymangia/MainProgram.java
+++ Classe_stream/src/main/java/org/tommymangia/MainProgram.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MainProgram {
 
     public static ArrayList<Punto> punti = new ArrayList();
