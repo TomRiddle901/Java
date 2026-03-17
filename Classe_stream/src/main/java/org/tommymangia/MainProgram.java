813be4153c25530dee50b337ae0d6a64674045a1
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-17 15:07:31 +0100

    Added more Punto

--- Classe_stream/src/main/java/org/tommymangia/MainProgram.java
+++ Classe_stream/src/main/java/org/tommymangia/MainProgram.java
@@ -8,15 +8,28 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class MainProgram {
     public static void main(){
-        Punto p = new Punto(3.78, 8.67, "A");
+        Punto p1 = new Punto(3.78, 8.67, "A");
+        Punto p2 = new Punto(3.21, 67, "B");
+        Punto p3 = new Punto(312, 57, "C");
+        Punto p4 = new Punto(809, 2319, "D");
+        Punto p5 = new Punto(83092, 34780, "E");
+
+//        try{
+//            FileWriter f = new FileWriter("out.txt");
+//            BufferedWriter bf = new BufferedWriter(f);
+//            bf.write(p.toString());
+//            bf.close();
+//        }catch (IOException ex){
+//            System.err.println("Errore input/output: " + ex.getMessage());
+//        }
+//
+      }
+
+    private static void export(Punto p){
         try{
-            FileWriter f = new FileWriter("out.txt");
-            BufferedWriter bf = new BufferedWriter(f);
-            bf.write(p.toString());
-            bf.close();
+
         }catch (IOException ex){
             System.err.println("Errore input/output: " + ex.getMessage());
         }
-
     }
 }
