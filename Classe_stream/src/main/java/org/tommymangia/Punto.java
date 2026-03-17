051a92f78cf1c2bd91aeebdaa11e5b20ae2bd07f
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-17 14:42:51 +0100

    Created a class for testing

--- Classe_stream/src/main/java/org/tommymangia/Punto.java
+++ Classe_stream/src/main/java/org/tommymangia/Punto.java
@@ -1,4 +1,13 @@
 package org.tommymangia;
 
 public class Punto {
+    private double x;
+    private double y;
+    private String nome;
+
+    public Punto(double x, double y, String nome){
+        this.x = x;
+        this.y = y;
+        this.nome = nome;
+    }
 }
