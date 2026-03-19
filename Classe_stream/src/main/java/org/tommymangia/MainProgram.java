d7812e2b9915e217d69710275be69a6a0e6796a4
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-19 13:11:17 +0100

    Initialized array `punti`: solved error `NullPointerException`

--- Classe_stream/src/main/java/org/tommymangia/MainProgram.java
+++ Classe_stream/src/main/java/org/tommymangia/MainProgram.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class MainProgram {
 
-    public static ArrayList<Punto> punti;
+    public static ArrayList<Punto> punti = new ArrayList<>();
 
     public static void main(String[] args){
         int scelta;
