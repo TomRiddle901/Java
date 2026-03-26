ef8b906ea3410010970f248c6a0c690c54184990
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-03-26 13:01:29 +0100

    New Project

---
+++ Test_Form/src/main/java/org/tommymangia/MainProgram.java
@@ -0,0 +1,9 @@
+package org.tommymangia;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MainProgram {
+    public static void main(String[] args) {
+
+    }
+}
