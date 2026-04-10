fa816f223c4151970728b810e70b9df7a27cff71
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-10 10:18:48 +0200

    Created a class for school courses

---
+++ Gestione_Scuola/src/main/java/org/tommymangia/gestione_scuola/ClassiScuola.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.tommymangia.gestione_scuola;
+
+/**
+ *
+ * @author tommaso
+ */
+public class ClassiScuola {
+    private int anno;
+    private String corso;
+    private String dipartimento;
+
+    public ClassiScuola(int anno, String corso, String dipartimento) {
+        this.anno = anno;
+        this.corso = corso;
+        this.dipartimento = dipartimento;
+    }
+    
+    
+}
