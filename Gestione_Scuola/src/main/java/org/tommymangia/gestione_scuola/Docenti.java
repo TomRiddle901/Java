7c701b9f92f758b23b75c0083ed816197556fec8
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-10 10:55:28 +0200

    Created `Docenti` class

---
+++ Gestione_Scuola/src/main/java/org/tommymangia/gestione_scuola/Docenti.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.tommymangia.gestione_scuola;
+
+/**
+ *
+ * @author tommaso
+ */
+public class Docenti {
+    private String nome;
+    private String cognome;
+    private String dipartimento;
+
+    public Docenti(String nome, String cognome, String dipartimento) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dipartimento = dipartimento;
+    }
+    
+}
