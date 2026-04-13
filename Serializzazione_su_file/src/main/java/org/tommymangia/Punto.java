153832fa00c8ca43cb76a992c5b3445536a4f32d
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-04-13 16:41:55 +0200

    Created class `Punto.java`

--- Serializzazione_su_file/src/main/java/org/tommymangia/Punto.java
+++ Serializzazione_su_file/src/main/java/org/tommymangia/Punto.java
@@ -1,4 +1,25 @@
 package org.tommymangia;
 
-public class Punto {
+import java.io.Serializable;
+
+public class Punto implements Serializable{
+    private static final long serialVersionUID = 1;
+    private double x;
+    private double y;
+    private String nome;
+
+    public Punto(double x, double y, String nome){
+        this.x = x;
+        this.y = y;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString(){
+        return nome + "(" + x + ";" + y + ")";
+    }
+
+    public String toCSV(){
+        return nome + ";" + x + ";" + y;
+    }
 }
