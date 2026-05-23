7e30d5dfea7abb01cbfe2fad0a4fe8c26e5e6a51
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-23 14:02:01 +0200

    Aggiunti attributi e cotruttore

--- Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Progetto.java
+++ Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Progetto.java
@@ -1,4 +1,20 @@
 package org.tommymangia;
 
-public class Progetto {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Progetto implements Serializable{
+    private String nome;
+    private LocalDate dataInizio;
+    private LocalDate dataFine;
+    private int avanzamento;
+    private ArrayList<Dipendente> dipendenti;
+
+    public Progetto(String nome, LocalDate dataInizio, LocalDate dataFine, int avanzamento){
+        this.nome = nome;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.avanzamento = avanzamento;
+    }
 }
