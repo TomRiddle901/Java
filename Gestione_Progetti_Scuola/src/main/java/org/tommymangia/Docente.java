812a9b03568bd68aae812a1b2b0b4dd003f9bee8
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-23 13:10:22 +0200

    Aggiunti attributi e costruttore nella classe `Docente`

--- Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Docente.java
+++ Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Docente.java
@@ -1,4 +1,13 @@
 package org.tommymangia;
 
-public class Docente {
+import java.time.LocalDate;
+
+public class Docente extends Dipendente{
+    private String materia;
+    private static float costoOrario = 60;
+
+    public Docente(String nome, String cognome, LocalDate dataNascita, char sesso, LocalDate dataAssunzione, int nOre, String materia){
+        super(nome, cognome, dataNascita, sesso, dataAssunzione, nOre);
+        this.materia = materia;
+    }
 }
