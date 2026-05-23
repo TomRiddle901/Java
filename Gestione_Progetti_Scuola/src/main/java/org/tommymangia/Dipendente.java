f1e7edc41140428c0fd4f52a8bff6dff7f0cb112
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-23 13:05:37 +0200

    Aggiunti attributi per la classe Dipendente e aggiunto costruttore

--- Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Dipendente.java
+++ Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Dipendente.java
@@ -3,5 +3,19 @@ package org.tommymangia;
 import java.time.LocalDate;
 
 public abstract class Dipendente {
+    protected String nome;
+    protected String cognome;
+    protected LocalDate dataNascita;
+    protected char sesso;
+    protected LocalDate dataAssunzione;
+    protected int nOre;
 
+    public Dipendente(String nome, String cognome, LocalDate dataNascita, char sesso, LocalDate dataAssunzione, int nOre){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.dataNascita = dataNascita;
+        this.sesso = sesso;
+        this.dataAssunzione = dataAssunzione;
+        this.nOre = nOre;
+    }
 }
