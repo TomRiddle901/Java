fbd90e44b0f16f690592107928bc68f9d2fa5bdb
TomRiddle901 <tommaso.mangialardo@hotmail.com>
2026-05-23 13:56:18 +0200

    Aggiunto metodo per calcolare il costo orario, attributi e construttore

--- Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Tecnico.java
+++ Gestione_Progetti_Scuola/src/main/java/org/tommymangia/Tecnico.java
@@ -1,4 +1,19 @@
 package org.tommymangia;
 
-public class Tecnico {
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Tecnico extends Dipendente{
+    private String area;
+    private static float cstoOrario = 40;
+
+    public Tecnico(String nome, String cognome, LocalDate dataNascita, char sesso, LocalDate dataAssunzione, int nOre, String area){
+        super(nome, cognome, dataNascita, sesso, dataAssunzione, nOre);
+        this.area = area;
+    }
+
+    @Override
+    public float calcolaCosto(){
+
+    }
 }
